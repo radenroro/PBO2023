2d3d7132ee2cea1de4370c2381e9280903943db3
radenroro <148857302+radenroro@users.noreply.github.com>
2023-11-05 20:46:08 +0700

    PBO Pertemuan Ke-5 & Ke-6

---
+++ TampilanGUI/src/tampilangui/Makeup.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tampilangui;
+
+/**
+ *
+ * @author Lenovo
+ */
+class Makeup {
+    private String Produk;
+    private String Harga;
+
+    /**
+     * @return the Produk
+     */
+    public String getProduk() {
+        return Produk;
+    }
+
+    /**
+     * @param Produk the Produk to set
+     */
+    public void setProduk(String Produk) {
+        this.Produk = Produk;
+    }
+
+    /**
+     * @return the Harga
+     */
+    public String getHarga() {
+        return Harga;
+    }
+
+    /**
+     * @param Harga the Harga to set
+     */
+    public void setHarga(String Harga) {
+        this.Harga = Harga;
+    }
+}
